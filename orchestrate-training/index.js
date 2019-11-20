6ae0e37ab999362e19de31f3d537fa4a3867ed88
Simona Cotin <simona.cotin@gmail.com>
2019-11-20 23:54:19 +0200

    🐶 First puppy rodeo

---
+++ orchestrate-training/index.js
@@ -0,0 +1,43 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an HTTP starter function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable activity function (default name is "Hello")
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your 
+ *    function app in Kudu
+ */
+
+const df = require("durable-functions");
+
+module.exports = df.orchestrator(function* (context) {
+    const outputs = [];
+
+    const input = context.df.getInput();
+    const puppyName = input.puppy;
+    if (!puppyName) {
+      throw new Error('A dog name is required as an input.');
+    }
+
+    const images = yield context.df.callActivity(
+        'search-images',
+        puppyName
+      );
+      const tag = yield context.df.callActivity(
+        'get-tag',
+        puppyName
+      );
+
+      const uploadImages = yield context.df.callActivity(
+        'upload-images',
+        {
+          id: tag.id,
+          urls: images
+        }
+      );
+
+    // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+    console.log(JSON.stringify(outputs))
+    return outputs;
+});
---
+++ search-images/index.js
@@ -0,0 +1,35 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an orchestrator function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable orchestration function
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *   function app in Kudu
+ */
+
+const axios = require('axios');
+const URL = 'https://api.cognitive.microsoft.com/bing/v7.0/images/search';
+async function getImages(context) {
+  try {
+    const response = await axios.get(URL, {
+      params: {
+        q: context.bindings.name,
+        imageType: 'photo'
+      },
+      headers: {
+        'Ocp-Apim-Subscription-Key': process.env.BING_API_KEY
+      }
+    });
+    return response.data.value;
+  } catch (error) {
+    context.log(`Error code: ${error.code} message: ${error.message}`);
+    throw new Error(error.message);
+  }
+}
+module.exports = async function(context) {
+  context.log(`>>>>>>>> Searching for images using search term: ${context.bindings.name}!`);
+  const images = await getImages(context);
+  return images;
+};
